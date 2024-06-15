876f28c11743bc483b6c7efdaf58b9bbdd145fa9
Lidor Cohen <lidorcohen.p1@gmail.com>
2024-06-15 19:45:47 +0300

    added animations to navbar links

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,14 +1,32 @@
 let cardElements = document.querySelectorAll(".brightness-animate-element");
+let regularButtons = document.querySelectorAll(".nav-regular-button");
 
-function startAnimation() {
+// BRIGHTNESS ANIMATION
+function startBrightnessAnimation() {
   this.classList.add("brightness-animating");
 }
 
-function stopAnimation() {
+function stopBrightnessAnimation() {
   this.classList.remove("brightness-animating");
 }
 
 cardElements.forEach((element) => {
-  element.addEventListener("mouseover", startAnimation);
-  element.addEventListener("mouseout", stopAnimation);
+  element.addEventListener("mouseover", startBrightnessAnimation);
+  element.addEventListener("mouseout", stopBrightnessAnimation);
+});
+
+// BUTTON HOVER ANIMATION
+// 1. REGULAR BUTTON
+
+function startRegularButtonAnimation() {
+  this.classList.add("regular-button-animating");
+}
+
+function stopRegularButtonAnimation() {
+  this.classList.remove("regular-button-animating");
+}
+
+regularButtons.forEach((element) => {
+  element.addEventListener("mouseover", startRegularButtonAnimation);
+  element.addEventListener("mouseout", stopRegularButtonAnimation);
 });
